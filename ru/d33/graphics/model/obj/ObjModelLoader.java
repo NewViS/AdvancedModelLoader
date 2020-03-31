5024739d3d561347adae2efeb88e6ab9a8a36e11
NewViS <victor98510@yandex.ru>
2020-03-31 17:03:06 +0400

    Advanced Model Loader for 1.12.2

---
+++ ru/d33/graphics/model/obj/ObjModelLoader.java
@@ -0,0 +1,30 @@
+package ru.d33.graphics.model.obj;
+
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.obj.WavefrontObject;
+import ru.d33.graphics.model.IModelCustom;
+import ru.d33.graphics.model.IModelCustomLoader;
+import ru.d33.graphics.model.ModelFormatException;
+
+public class ObjModelLoader implements IModelCustomLoader
+{
+
+    @Override
+    public String getType()
+    {
+        return "OBJ model";
+    }
+
+    private static final String[] types = { "obj" };
+    @Override
+    public String[] getSuffixes()
+    {
+        return types;
+    }
+
+    @Override
+    public IModelCustom loadInstance(ResourceLocation resource) throws ModelFormatException
+    {
+        return new WavefrontObject(resource);
+    }
+}
